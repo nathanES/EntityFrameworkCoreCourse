de2209f6c1f3a446da2588bda2ac50a110c7a9da
nathan <nathan.esslinger67@gmail.com>
2024-05-13 20:01:40 +0200

    first commit

---
+++ Dometrain.EFCore.API/Data/MoviesContext.cs
@@ -0,0 +1,23 @@
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dometrain.EFCore.API.Data;
+
+public class MoviesContext : DbContext
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("""
+                                    Data Source=localhost;
+                                    Initial Catalog=MoviesDB;
+                                    User Id=SA;
+                                    Password=MySaPassword123;
+                                    TrustServerCertificate=True;
+                                    """);
+        // Not proper logging
+        optionsBuilder.LogTo(Console.WriteLine);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
